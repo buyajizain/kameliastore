b994be0eac9354fa116457cf37d6d3087c967998
Irvan <buyajizain@gmail.com>
2026-09-23 19:59:09 +0700

    meta pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,22 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.loadNpmTasks:n.queue.push(arguments)};
+            if(!f._q)return;n.push(1000);;n.loaded=!0;n.version='2.0';
+            n.queue=[];script=document.createElement('script');
+            script.async=!0;script.src=v;s=document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(script,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '695641838885394');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=695641838885394&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Meta Pixel Code -->
     </head>
     <body class="font-sans antialiased h-full text-gray-900 bg-gray-50" x-data="{ sidebarOpen: false }">
         <div class="min-h-screen flex">
